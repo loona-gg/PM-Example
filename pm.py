1426e5c42373dfd47adc3beeed22c40ffbcad9e8
Artucuno <28388670+Artucuno@users.noreply.github.com>
2023-11-08 12:16:06 +1100

    Update pm.py

--- pm.py
+++ pm.py
@@ -1,5 +1,5 @@
 """
-Made by https://artucuno.dev for Loona.gg
+https://loona.gg
 """
 
 import urllib.parse
@@ -8,11 +8,12 @@ from typing import List, Union, Tuple
 import requests
 
 
-class MyPlugin(PMPlugin):
+class SpigotManager(PMPlugin):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.WEBSITE_URL = "https://www.spigotmc.org/"
         self.API_ENDPOINT = "https://api.spiget.org/v2/"
+        self.DEFAULT_EXT = "jar"
 
     def query(self, query: str, page: int = 0, limit: int = 25) -> Union[List[PluginQueryResult] | None, Tuple[List[PluginQueryResult], int] | None]:
         """
@@ -24,6 +25,7 @@ class MyPlugin(PMPlugin):
         """
         query = urllib.parse.quote(query)
         url = f"{self.API_ENDPOINT}search/resources/{query}?size={limit}&page={page}"
+        print(url)
         r = requests.get(url)
         if r.status_code == 200:
             js = r.json()
@@ -31,12 +33,13 @@ class MyPlugin(PMPlugin):
             for result in js:
                 results.append(PluginQueryResult(
                     id=result["id"],
+                    filename=f"{result['id']}.jar",
                     name=result["name"],
-                    description=result["description"] if "description" in result else None,
-                    tags=result["tag"],
+                    description=result["tag"] if "tag" in result else "",
+                    tags=[],
                     premium=result["premium"] if "premium" in result else False,
-                    icon=result["icon"]["data"],
-                    size=result["file"]["size"],
+                    icon=result["icon"]["data"] if result["icon"]["data"] else None,
+                    size=int(result["file"]["size"]) * 1000,  # Convert to bytes
                     views=result["views"] if "views" in result else 0,
                     likes=result["likes"] if "likes" in result else 0,
                     downloads=result["downloads"] if "downloads" in result else 0,
@@ -51,6 +54,38 @@ class MyPlugin(PMPlugin):
         else:
             return []
 
+    def get_plugin(self, plugin_id: Union[str, int]) -> PluginQueryResult:
+        """
+        Called when plugin is being installed
+        :param plugin_id: Plugin ID
+        :return: PluginQueryResult
+        """
+        url = f"{self.API_ENDPOINT}resources/{plugin_id}"
+        r = requests.get(url)
+        if r.status_code == 200:
+            result = r.json()
+            return PluginQueryResult(
+                id=result["id"],
+                filename=f"{result['id']}.jar",
+                name=result["name"],
+                description=result["tag"] if "tag" in result else "",
+                tags=[],
+                premium=result["premium"] if "premium" in result else False,
+                icon=result["icon"]["data"] if result["icon"]["data"] else None,
+                size=int(result["file"]["size"]) * 1000,  # Convert to bytes
+                views=result["views"] if "views" in result else 0,
+                likes=result["likes"] if "likes" in result else 0,
+                downloads=result["downloads"] if "downloads" in result else 0,
+                versions=result["testedVersions"] if "testedVersions" in result else [],
+                url=self.WEBSITE_URL + result["file"]["url"],
+                install_url=self.API_ENDPOINT + "resources/" + str(result["id"]) + "/download",
+                external=result["external"],
+                releaseDate=result["releaseDate"],
+                service_name="SpigotMC"
+            )
+        else:
+            return None
+
     def install(self, plugin: PluginQueryResult, server: str) -> None:
         """
         Called after plugin is installed
@@ -58,10 +93,9 @@ class MyPlugin(PMPlugin):
         :param server: UUID of server
         :return: None
         """
-        self.log(1, f"Plugin {plugin.name} (ID: {plugin.id}) was installed to {server}")
+        self.log(1, f"Plugin {plugin.name} (ID: {plugin.id}) was installed on {server}")
 
 
 def setup(*args, **kwargs):
-    """Called to load plugin - MUST RETURN PLUGIN CLASS"""
-    return MyPlugin(*args, **kwargs)
-
+    """Called to load plugin - MUST RETURN THE PLUGIN CLASS"""
+    return SpigotManager(*args, **kwargs)
